1a67e9cd80eff3157b8026051fd12dfdd1a4a0f6
mb <magnus.ben@gmail.com>
2019-12-05 21:59:20 +0100

    d1 part 2

--- 1/code.py
+++ 1/code.py
@@ -1,3 +1,14 @@
+def calculate_fuel_for_mass(mass):
+    return int((mass / 3.) - 2)
+
+def calculate_fuel(mass):
+    fuel = calculate_fuel_for_mass(mass)
+    return max(0,fuel) if fuel <= 2 else fuel + calculate_fuel(fuel) 
+
 with open('input.txt','r') as f:
-    print sum([ int(int(mass) / 3.) - 2 for mass in f.readlines()])
-    
+    masses = map(int,f.readlines())
+    fuel = sum([ calculate_fuel_for_mass(mass) for mass in masses])
+    print("part 1: {}".format(fuel))
+    fuel = sum([ calculate_fuel(mass) for mass in masses])
+    print("part 2: {}".format(fuel))
+
