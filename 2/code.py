aea2ec246deb0abe27472aab86da2ba7cf88cccd
mb <magnus.ben@gmail.com>
2019-12-06 23:17:39 +0100

    d2p2

--- 2/code.py
+++ 2/code.py
@@ -7,7 +7,19 @@ def process(program):
             program[k] = program[i] + program[j] if op == 1 else program[i] * program[j] 
 
 with open('input.txt','r') as f:
-    program = map(int, f.readlines()[0].split(','))
+    orig_program = map(int, f.readlines()[0].split(','))
+    program = orig_program[:]
     program[1]=12
     program[2]=2
     print process(program)
+
+    inputs = [(x, y) for x in xrange(100) for y in xrange(100)]
+    for noun, verb in inputs:
+        program = orig_program[:]
+        program[1]=noun
+        program[2]=verb
+        result = process(program)[0]
+        if result == 19690720:
+            print 100 * noun + verb, result
+
+            
